b1caa2d597077021a9c00c5888f48c37ed95fe00
Ezequiel Antoine <ezequielantoine@gmail.com>
2023-11-27 19:19:58 -0300

    Se comenzaron las actions

--- client/src/redux/action-types.js
+++ client/src/redux/action-types.js
@@ -2,5 +2,6 @@ export const GET_ALL_BREEDS='GET_ALL_BREEDS';
 export const GET_BY_NAME='GET_BY_NAME';
 export const GET_DETAIL='GET_DETAIL';
 export const POST_BREED='POST_BREED';
+export const GET_TEMPERAMENTS='GET_TEMPERAMENTS';
 export const FILTER='FILTER';
 export const ORDER='ORDER';
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,13 +1,63 @@
 /* Dependencies */
-import {GET_ALL_BREEDS,GET_DETAIL,GET_BY_NAME,POST_BREED,FILTER,ORDER} from './action-types';
+import {GET_ALL_BREEDS,GET_DETAIL,GET_BY_NAME,POST_BREED,GET_TEMPERAMENTS,FILTER,ORDER} from './action-types';
 import axios from 'axios';
+
+export const getAllBreeds=()=>{
+    return async (dispatch)=>{
+        try {
+            const {data}= await axios.get(`http://localhost:3001/dogs`);
+            return dispatch({
+                type:GET_ALL_BREEDS,
+                payload:data,
+            });    
+        } catch (error) {window.alert(error.message)};
+    }
+}
 
 export const getDetail=(id)=>{
     return async (dispatch)=>{
-        const {data}= axios.get(`http://localhost:3001/dogs/${id}`);
-        return dispatch({
-            type:GET_DETAIL,
-            payload:data,
-        });
+        try {
+            const {data}= await axios.get(`http://localhost:3001/dogs/${id}`);
+            return dispatch({
+                type:GET_DETAIL,
+                payload:data,
+            });    
+        } catch (error) {window.alert(error.message)}
     }
+}
+
+export const getBreedByName=(breed)=>{
+    return async (dispatch)=>{
+        try {
+            const {data}= await axios.get(`http://localhost:3001/dogs?name=${breed}`);
+            return dispatch({
+                type:GET_BY_NAME,
+                payload:data,
+            });    
+        } catch (error) {window.alert(error.message)};
+    }
+}
+
+export const createBreed=(breed, minHeight, maxHeight, minWeight, maxWeight, life_span, temperaments)=>{
+
+}
+
+export const getTemperaments=()=>{
+    return async (dispatch)=>{
+        try {
+            const {data}= await axios.get(`http://localhost:3001/temperaments`);
+            return dispatch({
+                type:GET_TEMPERAMENTS,
+                payload:data,
+            });    
+        } catch (error) {window.alert(error.message)};
+    }
+}
+
+export const filterBreeds=(gender)=>{
+    return {type: FILTER, payload:gender}
+}
+
+export const orderBreeds=(orden)=>{
+    return {type: ORDER, payload:orden}
 }
