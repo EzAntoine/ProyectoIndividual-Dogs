88e2804fd97a5cc42c0c75f3e2ae28e1348cd6eb
Ezequiel Antoine <ezequielantoine@gmail.com>
2023-11-29 19:45:38 -0300

    Se acomodaron estilos de Detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,3 +1,5 @@
+/* Styles */
+import style from './Detail.module.css'
 /* Hooks */
 import { useParams } from 'react-router-dom';
 import {useEffect, useState} from 'react';
@@ -5,7 +7,6 @@ import { useDispatch, useSelector } from 'react-redux';
 /* Dendencies */
 //import axios from 'axios';
 import { getDetail } from '../../redux/actions';
-/* Styles */
 
 
 const Detail=()=>{
@@ -24,15 +25,17 @@ const Detail=()=>{
     },[dispatch,id])
 
     return(
-        <div>
-            <div>
+        <div className={style.detailContainer}>
+            <div className={style.leftContent}>
                 <h2>ID: {dogDet.id && dogDet.id}</h2>
-                <img src={dogDet.image && dogDet.image} alt={dogDet.name}/>
                 <h2>Raza: {dogDet.name && dogDet.name}</h2>
                 <h2>Altura: {dogDet.height && dogDet.height}</h2>
                 <h2>Peso: {dogDet.weight && dogDet.weight}</h2>
                 <h2>Temperamento: {dogDet.temperament && dogDet.temperament}</h2>
                 <h2>Años de vida: {dogDet.life_span && dogDet.life_span}</h2>
+            </div>
+            <div className={style.rightContent}>
+                <img src={dogDet.image && dogDet.image} alt={dogDet.name}/>
             </div>
         </div>
     )
