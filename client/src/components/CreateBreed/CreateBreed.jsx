f9bcf5c51b9eaadd5d6be07319f7cd345b02fcb3
Ezequiel Antoine <ezequielantoine@gmail.com>
2023-11-29 23:48:13 -0300

    Se acomodaron validaciones, se agrego boton submit que se habilita solo al llenar todos los campos.

--- client/src/components/CreateBreed/CreateBreed.jsx
+++ client/src/components/CreateBreed/CreateBreed.jsx
@@ -23,40 +23,37 @@ const CreateBreed=()=>{
         maxWeight:"",
         life_span:"",
         temperament:"",
+        vacio:"Por favor llenar todos los campos.",
       });
 
       const validate=(input)=>{
-        if(!(/^[a-zA-Z]+$/).test(input.name))
-            setError({...error,name:"El nombre solo puede incluir letras."});
-        else
-            setError({...error,name:""});
-        
-        if(!(/^[0-9]+$/).test(input.minHeight) || input.minHeight>=input.maxHeight)
-            setError({...error,name:"La altura mínima solo puede ser un número y debe ser menor a la máxima."});
-        else
-            setError({...error,minHeight:""});
-
-        if(!(/^[0-9]+$/).test(input.maxHeight) || input.minHeight>=input.maxHeight)
-            setError({...error,name:"La altura máxima solo puede ser un número y debe ser mayor a la mínima."});
-        else
-            setError({...error,maxHeight:""});
-
-        if(!(/^[0-9]+$/).test(input.minWeight) || input.minWeight>=input.maxWeight)
-            setError({...error,name:"El peso mínimo solo puede ser un número y debe ser menor al máximo."});
-        else
-            setError({...error,minWeight:""});
-
-        if(!(/^[0-9]+$/).test(input.maxWeight) || input.minWeight>=input.maxWeight)
-            setError({...error,name:"El peso máximo solo puede ser un número y debe ser mayor al mínimo."});
-        else
-            setError({...error,maxWeight:""});
-
-
-            
-            
+        setError((err)=>({
+            ...err,
+            name:!(/^[a-zA-Z]+$/).test(input.name)
+                ? "El nombre solo puede incluir letras."
+                : "",
+            minHeight:
+                !(/^[0-9]+$/).test(input.minHeight) || input.minHeight >= input.maxHeight
+                  ? "La altura mínima solo puede ser un número y debe ser menor a la máxima."
+                  : "",
+            maxHeight:!(/^[0-9]+$/).test(input.maxHeight) || input.minHeight>=input.maxHeight
+                    ? "La altura máxima solo puede ser un número y debe ser mayor a la mínima."
+                    : "",
+            minWeight:!(/^[0-9]+$/).test(input.minWeight) || input.minWeight>input.maxWeight
+                    ? "El peso mínimo solo puede ser un número y debe ser menor al máximo."
+                    : "",
+            maxWeight:!(/^[0-9]+$/).test(input.maxWeight) || input.minWeight>=input.maxWeight
+                    ? "El peso máximo solo puede ser un número y debe ser mayor al mínimo."
+                    : "",
+            vacio:!input.name || !input.minHeight || !input.maxHeight || !input.minWeight || !input.maxWeight || !input.life_span || !input.temperament
+                    ? "Por favor llenar todos los campos."
+                    : "",
+        }))                           
       }
 
     const handleChange=(event)=>{
+        console.log(input);
+        console.log(error);
         setInput({
             ...input,
             [event.target.name]:event.target.value
@@ -69,7 +66,7 @@ const CreateBreed=()=>{
 
     return(
         <div>
-            <form onSubmit={""}>
+            <form /* onSubmit={""} */>
                 <div>
                     <label>Nombre de raza: </label>
                     <input type="text" name="name" value={input.value} onChange={handleChange}/>
@@ -78,29 +75,36 @@ const CreateBreed=()=>{
                 <div>
                     <label>Altura mínima: </label>
                     <input type="text" name="minHeight" value={input.value} onChange={handleChange}/>
+                    {<span>{error.minHeight}</span>}
                 </div>                
                 <div>
                     <label>Altura máxima: </label>
                     <input type="text" name="maxHeight" value={input.value} onChange={handleChange}/>
+                    {<span>{error.maxHeight}</span>}
                 </div>                
                 <div>
                     <label>Peso mínimo: </label>
                     <input type="text" name="minWeight" value={input.value} onChange={handleChange}/>
+                    {<span>{error.minWeight}</span>}
                 </div>
                 <div>
                     <label>Peso máximo: </label>
                     <input type="text" name="maxWeight" value={input.value} onChange={handleChange}/>
+                    {<span>{error.maxWeight}</span>}
                 </div>
                 <div>
                     <label>Años de vida: </label>
                     <input type="text" name="life_span" value={input.value} onChange={handleChange}/>
+                    {<span>{error.life_span}</span>}
                 </div>                
                 <div>
                     <label>Temperamento: </label>
                     <input type="text" name="temperament" value={input.value} onChange={handleChange}/>
+                    {<span>{error.temperament}</span>}
                 </div>
 
-                {/* <button onClick={handleSubmit} disabled={errors.e1 || errors.e2 || errors.e3 || errors.p1}>Submit</button> */}
+                {<span>{error.vacio}</span>}
+                {<button type="submit" disabled={error.vacio}>Submit</button>}
             </form>
         </div>
     )
