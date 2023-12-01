69c6ef82d2eee01666f915fffcde2bbcbe869b61
Ezequiel Antoine <ezequielantoine@gmail.com>
2023-11-30 23:45:14 -0300

    Se realizó el paginado, mostrando 8 razas por página.

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -11,26 +11,28 @@ const Cards=({allBreeds})=>{
     
     const [pagina,setPagina]=useState(1);
     const [dogPorPag,setDogPorPag]=useState(8);
-    const max=breeds.lenght/dogPorPag;
+    const max=Math.ceil(breeds.length/dogPorPag);
 
 
     return(
         <div className={style.cardsList}>
-            {breeds?.slice(
-                (pagina-1)*dogPorPag, (pagina-1)*dogPorPag + dogPorPag
-            ).map(({id,name,weight,temperament,image})=>{
-                if(id){
-                    return (<Card 
-                        key={id}
-                        id={id}
-                        name={name}
-                        weight={weight}
-                        temperament={temperament}
-                        image={image}
-                    />)
-                }else return "";
-            })
-            }
+            <div className={style.cardsListPage}>
+                {breeds?.slice(
+                    (pagina-1)*dogPorPag, (pagina-1)*dogPorPag + dogPorPag
+                ).map(({id,name,weight,temperament,image})=>{
+                    if(id){
+                        return (<Card 
+                            key={id}
+                            id={id}
+                            name={name}
+                            weight={weight}
+                            temperament={temperament}
+                            image={image}
+                        />)
+                    }else return "";
+                })
+                }
+            </div>
 
             <Paginate pagina={pagina} setPagina={setPagina} max={max}/>
         </div>
--- client/src/components/Paginate/Paginate.jsx
+++ client/src/components/Paginate/Paginate.jsx
@@ -1,11 +1,31 @@
 /* Styles */
+import { useState } from 'react';
 import style from './Paginate.module.css';
 
 export const Paginate=({pagina,setPagina,max})=>{
-    
-    return(
-        <div>
 
+    const [input,setInput]=useState(1);
+
+    const nextPage=()=>{
+        if(input<max){
+            setInput(input+1);
+            setPagina(pagina+1);
+        }
+    }
+
+    const prevPage=()=>{
+        if(input>1){
+            setInput(input-1);
+            setPagina(pagina-1);
+        }
+    }
+
+    return(
+        <div className={style.container}>
+            <button className={style.button} onClick={prevPage}> &lt; </button>
+            <input className={style.input} name='page' autoComplete='off' value={input}/>
+            <p> - {max}</p>
+            <button className={style.button} onClick={nextPage}> &gt; </button>
         </div>
     )
 }
